064c1ddd5aebcbe8222df430761042baa229ec10
Mehmet Anıl Demirci <36387091+AnLDemirci@users.noreply.github.com>
2021-10-19 00:01:59 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,20 @@
+console.log("Hello World");
+
+let dolarBugun = 9.32
+let dolarDun = 9.25
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+console.log(euroDun);
+
+let konutKredileri = ["Konut kredisi", "Emlak Konut Kredisi",10, "Kamu Konut Kredisi"]
+
+console.log(konutKredileri)
+
+konutKredileri.forEach(element => {
+    console.log(element)
+});
